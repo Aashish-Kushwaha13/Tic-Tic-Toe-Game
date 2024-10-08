a900975f2c1070fab68385b17913c6fefbcf41d7
Aashish-Kushwaha <kushwahaaashish2022@gmail.com>
2024-10-08 17:42:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TicTacToe from './Components/Tic Tac Toe/TicTacToe'
+
+function App() {
+  return (
+    <>
+    <TicTacToe/>
+    </>
+  )
+}
+
+export default App
